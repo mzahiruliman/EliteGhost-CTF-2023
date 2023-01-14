fbc3a04bdc27d5ad6bca20d8d4a49b9f4ed51ffa
Zahirul Iman <73564674+mzahiruliman@users.noreply.github.com>
2023-01-14 18:37:29 +0800

    Add files via upload

---
+++ Ordered/flag.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    char A = 1 + 0;
+    char E = H + 5;
+    char B = E + 2;
+    char J = "}" + 10;
+    char F = 4 + 6;
+    char I = R + 9;
+    char D = "{" + 4;
+    char G = X + 7;
+    char C = G + 3;
+    char H = 0 + 8;
+    char FLAG = "?";
+    }
